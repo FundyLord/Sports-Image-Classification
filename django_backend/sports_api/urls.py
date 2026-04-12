e3122854afb8b4cdff75e9345a69bed96e7d9471
FundyLord <ygjadhav04@gmail.com>
2026-04-12 22:50:05 +0530

    Fix metrics endpoint routing

--- django_backend/sports_api/urls.py
+++ django_backend/sports_api/urls.py
@@ -8,12 +8,12 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('classifier.urls')),
     # JWT Authentication endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include('django_prometheus.urls')),
 ]
 
 # Serve media files in development
